4ba3888677d2b7c8e7751f619de2e4f20353d224
Odd Eirik Igland <oddeirik.igland@gmail.com>
2019-08-08 10:54:48 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+
+"""
+    Segment Analyser
+"""
+
+from setuptools import setup, find_packages  # noqa: H301
+
+NAME = "segment-analyzer"
+VERSION = "1.0.0"
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = ["black", "pre-commit"]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="Segment Analyzer",
+    author_email="",
+    url="",
+    keywords=["Segment", "Analyze"],
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    long_description="""\
+    Segment Analyzer
+    """,
+)
