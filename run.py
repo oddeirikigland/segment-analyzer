e93657d27ccf22d77afd20d0d4881132cdd773e0
Odd Eirik Igland <oddeirik.igland@gmail.com>
2019-08-09 15:54:56 +0200

    rename html folder

--- run.py
+++ run.py
@@ -28,14 +28,14 @@ def not_found(error):
 @app.route("/")
 def index():
     """ static files serve """
-    return send_from_directory("dist", "index.html")
+    return send_from_directory("pages", "index.html")
 
 
 @app.route("/<path:path>")
 def static_proxy(path):
     """ static folder serve """
     file_name = path.split("/")[-1]
-    dir_name = os.path.join("dist", "/".join(path.split("/")[:-1]))
+    dir_name = os.path.join("pages", "/".join(path.split("/")[:-1]))
     return send_from_directory(dir_name, file_name)
 
 
